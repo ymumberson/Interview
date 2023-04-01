8db00685f5c71c64afa5bb5d88a8fc36718b86ca
Yoshan Mumberson <mryosh@outlook.com>
2023-04-01 15:43:53 +0100

    Added a ghost for placement
    Ghost has an alpha of 0.5f

--- Assets/Scripts/Grid/GridBuilder.cs
+++ Assets/Scripts/Grid/GridBuilder.cs
@@ -11,6 +11,7 @@ public class GridBuilder : MonoBehaviour
     [SerializeField] LayerMask detect_layer;
     public Building.Direction rotation;
     private GameObject selected_prefab;
+    private Building ghost;
 
     private int height;
     private int width;
@@ -23,19 +24,34 @@ public class GridBuilder : MonoBehaviour
         cell_size = 10f;
         WorldGrid = new Grid(height, width, cell_size);
         selected_prefab = prefab_list[0];
+
+        ghost = Instantiate(selected_prefab, Vector3.zero, Quaternion.Euler(0, Building.GetDirectionAngle(rotation), 0)).GetComponent<Building>();
+        ghost.SetDirection(rotation);
+        ghost.SetCellSize(cell_size);
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        RaycastHit hit;
+        Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000f, detect_layer);
+        Vector2Int click_position = Vector2Int.zero;
+        Vector3 world_position = Vector3.zero;
+        if (hit.rigidbody != null)
         {
-            RaycastHit hit;
-            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000f, detect_layer);
+            //Debug.Log("Clicked at coordinate: " + hit.point + " == " + WorldGrid.GetXY(hit.point));
+            click_position = WorldGrid.GetXY(hit.point);
+            world_position = WorldGrid.GetWorldPosition(click_position);
+        }
+
+            if (Input.GetMouseButtonDown(0))
+        {
+            //RaycastHit hit;
+            //Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000f, detect_layer);
             if (hit.rigidbody != null)
             {
                 //Debug.Log("Clicked at coordinate: " + hit.point + " == " + WorldGrid.GetXY(hit.point));
-                Vector2Int click_position = WorldGrid.GetXY(hit.point);
-                Vector3 world_position = WorldGrid.GetWorldPosition(click_position);
+                //Vector2Int click_position = WorldGrid.GetXY(hit.point);
+                //Vector3 world_position = WorldGrid.GetWorldPosition(click_position);
 
                 GameObject building = Instantiate(selected_prefab, Vector3.zero, Quaternion.Euler(0, Building.GetDirectionAngle(rotation), 0));
                 Building building_script = building.GetComponent<Building>();
@@ -57,19 +73,18 @@ public class GridBuilder : MonoBehaviour
 
         if (Input.GetMouseButtonDown(1))
         {
-            RaycastHit hit;
-            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000f, detect_layer);
+            //RaycastHit hit;
+            //Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000f, detect_layer);
             if (hit.rigidbody != null)
             {
                 //Debug.Log("Clicked at coordinate: " + hit.point + " == " + WorldGrid.GetXY(hit.point));
-                Vector2Int click_position = WorldGrid.GetXY(hit.point);
-                Vector3 world_position = WorldGrid.GetWorldPosition(click_position);
+                //Vector2Int click_position = WorldGrid.GetXY(hit.point);
+                //Vector3 world_position = WorldGrid.GetWorldPosition(click_position);
 
                 Building b = WorldGrid.GetBuilding(click_position.x, click_position.y);
 
                 if (b != null)
                 {
-                    string s = "Building occupies";
                     foreach (Vector2Int v in b.GetOccupiedTiles(WorldGrid))
                     {
                         WorldGrid.RemoveBuilding(v.x, v.y);
@@ -81,11 +96,30 @@ public class GridBuilder : MonoBehaviour
         if (Input.GetKeyDown("r"))
         {
             this.rotation = Building.NextDirection(this.rotation);
+            UpdateGhost();
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha1)) selected_prefab = prefab_list[0];
-        if (Input.GetKeyDown(KeyCode.Alpha2)) selected_prefab = prefab_list[1];
-        if (Input.GetKeyDown(KeyCode.Alpha3)) selected_prefab = prefab_list[2];
-        if (Input.GetKeyDown(KeyCode.Alpha4)) selected_prefab = prefab_list[3];
+        if (Input.GetKeyDown(KeyCode.Alpha1)) selected_prefab = prefab_list[0]; UpdateGhost();
+        if (Input.GetKeyDown(KeyCode.Alpha2)) selected_prefab = prefab_list[1]; UpdateGhost();
+        if (Input.GetKeyDown(KeyCode.Alpha3)) selected_prefab = prefab_list[2]; UpdateGhost();
+        if (Input.GetKeyDown(KeyCode.Alpha4)) selected_prefab = prefab_list[3]; UpdateGhost();
+
+        ghost.SetPosition(world_position);
+    }
+
+    private void UpdateGhost()
+    {
+        if (ghost != null) Destroy(ghost.gameObject);
+        ghost = Instantiate(selected_prefab, Vector3.zero, Quaternion.Euler(0, Building.GetDirectionAngle(rotation), 0)).GetComponent<Building>();
+        ghost.SetDirection(rotation);
+        ghost.SetCellSize(cell_size);
+
+        Renderer r = ghost.gameObject.GetComponentInChildren<Renderer>();
+        if (r != null)
+        {
+            Color c = r.material.color;
+            c.a = 0.5f;
+            r.material.color = c;
+        }
     }
 }
